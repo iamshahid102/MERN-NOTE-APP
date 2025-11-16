6a4e1b4b67faf78e1677479f98fe72430fb8a9ac
Muhammad Shahid <iamshahid102@gmail.com>
2025-11-17 00:16:41 +0500

    frontend updated

--- Backend/index.js
+++ Backend/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: `${process.env.FRONTEND_URL}`,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
